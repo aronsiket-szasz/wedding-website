ef25b0454e8b6dac40b5322cba092206c3d7a6b8
aronsiket-szasz <siketszasz.aron@gmail.com>
2025-02-21 18:52:47 +0200

    Kalenteri

--- js/scripts.js
+++ js/scripts.js
@@ -201,23 +201,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Áronin ja Lotan häät",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('Jul 19, 2025 13:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('Jul 19, 2025 23:59'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Aitoniementie 268, 33680 Tampere',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "Lämpimästi tervetuloa todistamanaa Áronin ja Lotan hääpäivää sekä seuraamaan jännityksellä sanommeko toisillemme 'tahdon'. Mikäli teillä tulee kysyttävää voitte olla yhteydessä Ároniin numerolla 0404160951."
         }
     });
 
